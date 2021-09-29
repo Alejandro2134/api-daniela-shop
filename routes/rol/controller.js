71c72d29e5307a3ae17184bcfebe66c77a3edfb9
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-29 13:40:56 -0500

    Requerimiento [Crear roles]

--- routes/rol/controller.js
+++ routes/rol/controller.js
@@ -2,14 +2,12 @@ const store = require('./store');
 
 const addRole = newRole => {
     return new Promise((resolve, reject) => {
-        if(newRole.rol === '') {
-            reject('El rol no puede estar vacio');
-            return false;
+        if(!newRole.rol) {
+            return reject('El rol no puede estar vacio');
         } else {
             store.addRole(newRole)
                 .catch(err => {
-                    reject(err);
-                    return false;
+                    return reject(err);
                 })
 
             resolve('');
