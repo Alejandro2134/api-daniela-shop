972ebd910ba248ffeb77357e1da952f7feb91288
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-29 20:49:02 -0500

    Db

--- db.js
+++ db.js
@@ -17,7 +17,7 @@ const connectionDetails = {
 
 const db = pgp(connectionDetails);
 
-const initializeDb = () => {
+const initializeDb = async () => {
     products.map(async product => {
         try {
             await db.none('INSERT INTO products(product_id, product_name, description, price) VALUES ($(id), $(name), $(description), $(price))', {
@@ -30,6 +30,23 @@ const initializeDb = () => {
             console.log('Ya se inicializo la base de datos con los productos iniciales');
         }
     });
+
+    try {
+        await db.none('INSERT INTO roles(roles_id, rol_name) VALUES ($(id), $(name))', {
+            id: '29f1cbcb-fd0b-470a-823b-e1c237c7bdae',
+            name: 'admin'
+        })
+
+        await db.none('INSERT INTO users(user_document, last_name, user_name, roles_id, user_id) VALUES ($(document), $(last_name), $(user_name), $(roles_id), $(user_id))', {
+            document: '1245668954',
+            last_name: 'Zapata',
+            user_name: 'Alejandro',
+            roles_id: '29f1cbcb-fd0b-470a-823b-e1c237c7bdae',
+            user_id: 'ea8b8d1b-651b-425d-8730-a01b34a6c9e2'
+        })
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = {
