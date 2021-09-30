ad00daf1def0b7df131d7106b4bbcce5adf9cbef
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-29 23:55:02 -0500

    Rutas y respuestas

--- network/routes.js
+++ network/routes.js
@@ -2,12 +2,14 @@ const rolRouter = require('../routes/rol/network');
 const userRouter = require('../routes/user/network');
 const productRouter = require('../routes/product/network');
 const saleRouter = require('../routes/sale/network');
+const accountingRouter = require('../routes/accounting/network');
 
 const routesApi = (app) => {
     app.use('/rol', rolRouter);
     app.use('/user', userRouter);
     app.use('/product', productRouter);
     app.use('/sale', saleRouter);
+    app.use('/accounting', accountingRouter);
 }
 
 module.exports = routesApi;
