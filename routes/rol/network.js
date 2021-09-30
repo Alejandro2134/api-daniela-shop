935c0dcd260fd2470997f2e9679a98298e85c03d
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-30 00:55:43 -0500

    Requerimiento [Crear roles]

--- routes/rol/network.js
+++ routes/rol/network.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../../auth');
 const controller = require('./controller');
 
 const { succes, error } = require('../../network/response');
 
 router.post('/', (req, res) => {
-    const { rol } = req.body;
-    controller.addRole(rol)
+    const { authorization } = req.headers;
+
+    auth(authorization)
         .then(response => {
-            succes(req, res, response, 201);
+            if(response.rol_name === 'admin') {
+                const { rol } = req.body;
+                controller.addRole(rol)
+                    .then(response => {
+                        succes(req, res, response, 201);
+                    })
+                    .catch(err => {
+                        error(req, res, err, 400, '');
+                    })
+            } else {
+                error(req, res, 'El rol que tiene este usuario no puede realizar esta acción', 400, '');
+            }
         })
         .catch(err => {
-            error(req, res, err, 400, '');
+            error(req, res, err.toString(), 400, '');
         })
 })
 
