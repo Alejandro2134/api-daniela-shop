a119c63982147a07076c586ef50ca4210e435db5
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-29 20:49:08 -0500

    Auth module

---
+++ auth.js
@@ -0,0 +1,14 @@
+const db = require('./db');
+
+const auth = async idUser => {
+    try {
+        const rolId = await db.db.one('SELECT roles_id FROM users WHERE user_id = $1', idUser);
+        const rol = await db.db.one('SELECT rol_name FROM roles WHERE roles_id = $1', rolId.roles_id);
+        return rol;
+    } catch (err) {
+        console.log(err);
+        throw new Error('No existe el usuario o no cuenta con rol, agreguele uno si es el caso');
+    }
+}
+
+module.exports = auth;
