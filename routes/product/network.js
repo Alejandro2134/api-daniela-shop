bf45c2dfe05deca042b4dfaacc6435368b336699
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-30 00:55:22 -0500

    Requerimiento [Crear, Listar]

--- routes/product/network.js
+++ routes/product/network.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('./../../auth');
+
 const controller = require('./controller');
 
 const { succes, error } = require('../../network/response');
 
 router.post('/', (req, res) => {
-    const { product } = req.body;
-    controller.addProduct(product)
+    const { authorization } = req.headers;
+
+    auth(authorization)
         .then(response => {
-            succes(req, res, response, 201);
+            if(response.rol_name === 'admin') {
+                const { product } = req.body;
+                controller.addProduct(product)
+                    .then(response => {
+                        succes(req, res, response, 201);
+                    })
+                    .catch(err => {
+                        error(req, res, err, 400, '');
+                    })
+            } else {
+                error(req, res, 'El rol que tiene este usuario no puede realizar esta acción', 400, '');
+            }  
         })
         .catch(err => {
-            error(req, res, err, 400, '');
+            error(req, res, err.toString(), 400, '');
         })
 })
 
 router.get('/', (req, res) => {
-    controller.listProducts()
+    const { authorization } = req.headers;
+
+    auth(authorization)
         .then(response => {
-            succes(req, res, response, 200);
+            controller.listProducts()
+                .then(response => {
+                    succes(req, res, response, 200);
+                })
+                .catch(err => {
+                    error(req, res, err, 500, '');
+                })
         })
         .catch(err => {
-            error(req, res, err, 500, '');
+            error(req, res, err.toString(), 400, '');
         })
 })
 
