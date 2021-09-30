e211fd6ae51b779deb99db93b887d99fad98ebe6
Alejandro Zapata <zapatamolinaalejandro@gmail.com>
2021-09-30 00:55:48 -0500

    Index

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const db = require('./db');
+const routesApi = require('./network/routes');
 
 const app = express();
+
+app.use(express.json());
+
+// Inicializar db con productos
+db.initializeDb();
+
+//Rutas api
+routesApi(app);
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
